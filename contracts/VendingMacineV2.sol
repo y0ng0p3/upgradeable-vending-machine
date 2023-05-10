ada0bcbdf1042cb56b731a81eae6aa5bb5861189
y0ng0p3 <vaillantyomen@gmail.com>
2023-05-10 20:15:08 +0100

    Add VendingMachineV2.

---
+++ contracts/VendingMacineV2.sol
@@ -0,0 +1,35 @@
+// SPX-Licence-Identifier: MIT
+pragma solidity 0.8.18;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract VendingMacineV2 is Initializable {
+    uint public numSodas;
+    address public owner;
+
+    function initialize(uint _numSodas)  public initializer {
+        numSodas = _numSodas;
+        owner = msg.sender;
+    }
+    
+    function purchaseSoda() public payable {
+        require(msg.value >= 1000 wei, "You must pay 1000 wei for a soda!");
+        require(numSodas > 0, "No soda to be purchased!");
+        numSodas--;
+    }
+
+    function withdrawProfits() public onlyOwner {
+        require(address(this).balance > 0, "Profits must be greater than 0 in order to withdraw.");
+        (bool sent, ) = owner.call{ value: address(this).balance }("");
+        require(sent, "Failed to send profits.");
+    }
+
+    function setNewOwner(address _newOwner) public onlyOwner {
+        owner = _newOwner;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function.");
+        _;
+    }
+}
